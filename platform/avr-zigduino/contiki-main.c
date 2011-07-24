69961e1c0719a7d14129d9b43c5c0de7dd87a059
maniacbug <maniacbug@ymail.com>
2011-07-23 21:04:57 -0700

    Fix warnings

--- platform/avr-zigduino/contiki-main.c
+++ platform/avr-zigduino/contiki-main.c
@@ -64,6 +64,8 @@
 #include "dev/serial-line.h"
 #include "dev/slip.h"
 
+#include "dev/leds.h"
+
 // Zigduino has not been tested with Rime.
 #if 0
 #if UIP_CONF_ROUTER&&0
@@ -78,6 +80,7 @@
 
 uint8_t mac_address[8] EEMEM = {0x02, 0x11, 0x22, 0xff, 0xfe, 0x33, 0x44, 0x55};
 
+/* Defined but not used
 uint8_t rf_channel[2] EEMEM = {26, ~26};
 
 static uint8_t get_channel_from_eeprom()
@@ -93,6 +96,7 @@ static uint8_t get_channel_from_eeprom()
 
   return 26;
 }
+*/
 
 static bool get_mac_from_eeprom(uint8_t* macptr)
 {
