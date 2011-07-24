b08038c3eb6f3f5a7e1ab1c8b0d98a38801e1dcc
maniacbug <maniacbug@ymail.com>
2011-07-23 21:08:15 -0700

    Added udp-ipv6 test

---
+++ platform/avr-zigduino/tests/udp-ipv6/tests.sh
@@ -0,0 +1,6 @@
+killall Python
+make udp-server.avr-zigduino.u -j10 || exit 1
+make udp-client.avr-zigduino.u AVRDUDE_PORT=/dev/tty.usbserial-A600eHIs || exit 1
+../../tools/runtest.py /dev/tty.usbserial-A4009YFR 
+#../../tools/runtest.py /dev/tty.usbserial-A600eHIs
+#killall Python 
