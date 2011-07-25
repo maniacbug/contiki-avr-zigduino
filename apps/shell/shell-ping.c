c9b7a29d20b7219dac89aa30c3eb7b224827ac1c
maniacbug <maniacbug@ymail.com>
2011-07-24 22:45:35 -0400

    Example shell ping now prints the send and shows the response.

--- apps/shell/shell-ping.c
+++ apps/shell/shell-ping.c
@@ -40,6 +40,17 @@
 #include "shell.h"
 #include "contiki-net.h"
 
+#define DEBUG 1
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF(" %02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x ", ((u8_t *)addr)[0], ((u8_t *)addr)[1], ((u8_t *)addr)[2], ((u8_t *)addr)[3], ((u8_t *)addr)[4], ((u8_t *)addr)[5], ((u8_t *)addr)[6], ((u8_t *)addr)[7], ((u8_t *)addr)[8], ((u8_t *)addr)[9], ((u8_t *)addr)[10], ((u8_t *)addr)[11], ((u8_t *)addr)[12], ((u8_t *)addr)[13], ((u8_t *)addr)[14], ((u8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF(" %02x:%02x:%02x:%02x:%02x:%02x ",lladdr->addr[0], lladdr->addr[1], lladdr->addr[2], lladdr->addr[3],lladdr->addr[4], lladdr->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#endif
+
 /*---------------------------------------------------------------------------*/
 PROCESS(shell_ping_process, "ping");
 SHELL_COMMAND(ping_command,
@@ -90,6 +101,12 @@ send_ping(uip_ipaddr_t *dest_addr)
   
   UIP_ICMP_BUF->icmpchksum = 0;
   UIP_ICMP_BUF->icmpchksum = ~uip_icmp6chksum();
+  
+  PRINTF("Sending Echo Request to");
+  PRINT6ADDR(&UIP_IP_BUF->destipaddr);
+  PRINTF("from");
+  PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
+  PRINTF("\n");
   
   tcpip_ipv6_output();
 }
@@ -144,6 +161,7 @@ PROCESS_THREAD(shell_ping_process, ev, data)
     PROCESS_EXIT();
   }
   uiplib_ipaddrconv(data, &remoteaddr);
+  icmp6_new(NULL);
 
   send_ping(&remoteaddr);
   
@@ -153,6 +171,18 @@ PROCESS_THREAD(shell_ping_process, ev, data)
     etimer_set(&e, CLOCK_SECOND * 10);
     
     PROCESS_WAIT_EVENT();
+  
+#if UIP_CONF_IPV6
+    if(ev == tcpip_icmp6_event) {
+      switch(*((uint8_t *)data)){
+      case ICMP6_ECHO_REPLY:
+	PRINTF("Echo reply received.\n");
+	break;
+      default:
+	PRINTF("Other ICMP6 message received.\n");
+      }
+    }
+#endif
 
     if(etimer_expired(&e)) {
       PROCESS_EXIT();      
