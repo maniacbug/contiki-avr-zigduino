0ffffa20df79401f2000d26e326432fe5cc54340
maniacbug <maniacbug@ymail.com>
2011-08-10 22:10:27 -0400

    Reverse order of bytes on originator address.  collect-view app sends the parent node up big endian, so the example should send up the originator big endian also.

--- examples/ipv6/rpl-collect/collect-common.c
+++ examples/ipv6/rpl-collect/collect-common.c
@@ -96,7 +96,7 @@ collect_common_recv(const rimeaddr_t *originator, uint8_t seqno, uint8_t hops,
   printf(" %lu %lu 0", ((time >> 16) & 0xffff), time & 0xffff);
   /* Ignore latency for now */
   printf(" %u %u %u %u",
-         originator->u8[0] + (originator->u8[1] << 8), seqno, hops, 0);
+         originator->u8[1] + (originator->u8[0] << 8), seqno, hops, 0);
   for(i = 0; i < payload_len / 2; i++) {
     memcpy(&data, payload, sizeof(data));
     payload += sizeof(data);
