776683f65a7db10b1c78a225e51535d82b47134c
maniacbug <maniacbug@ymail.com>
2011-08-15 14:31:08 -0400

    For compatability with collect-view app, the template should start with the SKY's sensors and ordering.

--- apps/collect-view/collect-view-template.c
+++ apps/collect-view/collect-view-template.c
@@ -1,9 +1,4 @@
-#include "collect-view.h"
-
-enum {
-  SENSOR1,
-  SENSOR2,
-};
+#include "collect-view-sky.h"
 
 /*---------------------------------------------------------------------------*/
 void
@@ -11,5 +6,16 @@ collect_view_arch_read_sensors(struct collect_view_data_msg *msg)
 {
   msg->sensors[SENSOR1] = 0;
   msg->sensors[SENSOR2] = 0;
+  msg->sensors[BATTERY_VOLTAGE_SENSOR] = 0;
+  msg->sensors[BATTERY_INDICATOR] = 0;
+  msg->sensors[LIGHT1_SENSOR] = 0;
+  msg->sensors[LIGHT2_SENSOR] = 0;
+  msg->sensors[TEMP_SENSOR] = 0;
+  msg->sensors[HUMIDITY_SENSOR] = 0;
+  msg->sensors[RSSI_SENSOR] = 0;
+  msg->sensors[ETX1_SENSOR] = 0;
+  msg->sensors[ETX2_SENSOR] = 0;
+  msg->sensors[ETX3_SENSOR] = 0;
+  msg->sensors[ETX4_SENSOR] = 0;
 }
 /*---------------------------------------------------------------------------*/
